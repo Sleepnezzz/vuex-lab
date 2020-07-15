a1993618df07c72896deaf1949420015055824c2
Thakun Uppathi <bbie.eng@gmail.com>
2020-07-15 13:23:59 +0700

    Initial dist subtree commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vuex-lab/precache-manifest.160005fd58e2545d0cbe907ce3128a43.js"
+  "precache-manifest.bb90ffc74e936f52c92bb5cb883223a3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-lab"});
