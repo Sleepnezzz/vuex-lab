06c15265013ed35f998244363e9d9dae8d8858d0
Thakun Uppathi <bbie.eng@gmail.com>
2020-07-15 12:43:16 +0700

    Initial dist subtree commit

---
+++ precache-manifest.ab0a1c92a3fa7fcac3cc2aa1b3b1e152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cead686b639bd0f02072",
+    "url": "/css/app.cc6e07a4.css"
+  },
+  {
+    "revision": "3790ce10c76d23ae4eb66182cd92aa2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f9580bd475aa70ec1b",
+    "url": "/js/about.fb0196df.js"
+  },
+  {
+    "revision": "cead686b639bd0f02072",
+    "url": "/js/app.5f7d63ab.js"
+  },
+  {
+    "revision": "5ffbe9522b9b0e7b9bab",
+    "url": "/js/chunk-vendors.9552a92f.js"
+  },
+  {
+    "revision": "d34b4e796c258365ad8a2c6f362292aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
